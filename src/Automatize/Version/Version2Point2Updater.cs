30d7d018d27b63fc59699cd82491eee2ed371cdd
Cookie1981 <david.cook@bglgroup.co.uk>
2019-01-09 13:20:00 +0000

    Upadted to support 2.2.102

--- src/Automatize/Version/Version2Point2Updater.cs
+++ src/Automatize/Version/Version2Point2Updater.cs
@@ -5,11 +5,11 @@
         public int MajorVersion => 2;
         public int MinorVersion => 2;
         public string CurrentFramework => "netcoreapp2.1";
-        public string MinimumCommonLibVersion => "1.0.310";
-        public string LinuxSdkImageVersion => "dotnet:2.2.100-sdk";
-        public string AlpineSdkImageVersion => "dotnet:2.2.100-sdk-alpine3.8";
-        public string LinuxRuntimeImageVersion => "dotnet:2.2.0-aspnetcore-runtime";
-        public string AlplineRuntimeImageVersion => "dotnet:2.2.0-aspnetcore-runtime-alpine3.8";
+        public string MinimumCommonLibVersion => "1.0.345";
+        public string LinuxSdkImageVersion => "dotnet:2.2.102-sdk";
+        public string AlpineSdkImageVersion => "dotnet:2.2.102-sdk-alpine3.8";
+        public string LinuxRuntimeImageVersion => "dotnet:2.2.1-aspnetcore-runtime";
+        public string AlplineRuntimeImageVersion => "dotnet:2.2.1-aspnetcore-runtime-alpine3.8";
         public string MonoVersion => "5.16.0.179";
 
         public string TargetFramework => $"netcoreapp{MajorVersion}.{MinorVersion}";
