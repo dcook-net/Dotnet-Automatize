9496a42369a5b8a2ec701414b49939c3c737db0a
Cookie1981 <david.cook@bglgroup.co.uk>
2019-01-25 11:13:40 +0000

    Updated for latest patch release of .Net Core

--- src/Automatize/Version/Version2Point2Updater.cs
+++ src/Automatize/Version/Version2Point2Updater.cs
@@ -6,8 +6,8 @@
         public int MinorVersion => 2;
         public string CurrentFramework => "netcoreapp2.1";
         public string MinimumCommonLibVersion => "1.0.345";
-        public string LinuxSdkImageVersion => "dotnet:2.2.102-sdk";
-        public string AlpineSdkImageVersion => "dotnet:2.2.102-sdk-alpine3.8";
+        public string LinuxSdkImageVersion => "dotnet:2.2.103-sdk";
+        public string AlpineSdkImageVersion => "dotnet:2.2.103-sdk-alpine3.8";
         public string LinuxRuntimeImageVersion => "dotnet:2.2.1-aspnetcore-runtime";
         public string AlplineRuntimeImageVersion => "dotnet:2.2.1-aspnetcore-runtime-alpine3.8";
         public string MonoVersion => "5.16.0.179";
