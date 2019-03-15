9f02473ea8e74c1831c40fdabbe6665d29044c48
Dave Cook <david.cook@bglgroup.co.uk>
2019-03-15 13:38:55 +0000

    2.1 not targets 2.1.9 runtime, 2.1.505 SDK and alpine3.9

--- src/Automatize/Version/Version2Point1Updater.cs
+++ src/Automatize/Version/Version2Point1Updater.cs
@@ -6,10 +6,10 @@
         public int MinorVersion => 1;
         public string CurrentFramework => "netcoreapp2.0";
         public string TargetFramework => $"netcoreapp{MajorVersion}.{MinorVersion}";
-        public string LinuxSdkImageVersion => "dotnet:2.1.500-sdk";
-        public string AlpineSdkImageVersion => "dotnet:2.1.500-sdk-alpine3.7";
-        public string LinuxRuntimeImageVersion => "dotnet:2.1.6-aspnetcore-runtime";
-        public string AlpineRuntimeImageVersion => "dotnet:2.1.6-aspnetcore-runtime-alpine3.7";
+        public string LinuxSdkImageVersion => "dotnet:2.1.505-sdk";
+        public string AlpineSdkImageVersion => "dotnet:2.1.505-sdk-alpine3.9";
+        public string LinuxRuntimeImageVersion => "dotnet:2.1.9-aspnetcore-runtime";
+        public string AlpineRuntimeImageVersion => "dotnet:2.1.9-aspnetcore-runtime-alpine3.9";
         public string MinimumCommonLibVersion => "1.0.310";
         public string MonoVersion => "5.16.0.179";
     }
